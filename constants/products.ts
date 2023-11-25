262ec184651037f4b2541ac063b3c7650b01d409
NastyaTsimanovich97 <43342938+NastyaTsimanovich97@users.noreply.github.com>
2023-11-25 16:23:20 +0300

    Task-3: First API with AWS API Gateway and AWS Lambda (#1)
    * feat(task-3): implement Products service
    
    * feat(task-3): update uri
    
    * fix(task-3): fix swagger server
    
    ---------
    
    Co-authored-by: anastasiya.tsymanovich <anastasiya.tsymanovich@zealicsolutions.com>

---
+++ constants/products.ts
@@ -0,0 +1,21 @@
+export const PRODUCTS = [{
+  id: '00000000-0000-0000-0000-000000000000',
+  title: 'Before Sunrise',
+  description: 'A young man and woman meet on a train in Europe, and wind up spending one evening together in Vienna. Unfortunately, both know that this will probably be their only night together. A young man and woman meet on a train in Europe, and wind up spending one evening together in Vienna.',
+  price: 2
+}, {
+  id: '00000000-1111-1111-1111-000000000000',
+  title: 'Before Sunset',
+  description: 'The film picks up the story in Before Sunrise of the young American man (Hawke) and French woman (Delpy) who spent a passionate night together in Vienna. Their paths intersect nine years later in Paris, and the film appears to take place in real time as they spend an afternoon together.',
+  price: 1
+}, {
+  id: '11111111-1111-1111-1111-000000000000',
+  title: 'Shrek',
+  description: 'Shrek is a large, green-skinned, physically intimidating ogre with a Scottish accent. In Shrek Forever After, however, it is revealed that he is much smaller than the average ogre.',
+  price: 5
+}, {
+  id: '11111111-0000-0000-0000-000000000000',
+  title: 'Inside Out',
+  description: 'Inside Out is an animated comedy about a girl named Riley (voice of Kaitlyn Dias), who is uprooted from her life in the US Midwest when her father gets a new job in San Francisco. Riley is largely guided by her emotions, each of which is shown as an actual character.',
+  price: 4
+}];
---
+++ resources/lambdas/product/index.ts
@@ -0,0 +1,26 @@
+import { PRODUCTS } from '../../../constants/products';
+
+import { buildResponseBody } from '../../../utils/buildResponseBody.util';
+
+export const handler = async function(event: any) {
+  try {
+    const method = event.httpMethod;
+    const productId = event.pathParameters.productId;
+
+    if (method === "GET") {
+      const product = PRODUCTS.find(item => item.id === productId);
+
+      if (product) {
+        return buildResponseBody(200, JSON.stringify(product));
+      }
+
+      return buildResponseBody(400, 'Product not found');
+    }
+
+    return buildResponseBody(400, 'We only accept GET not ' + method);
+  } catch(error: any) {
+    const body = error.stack || JSON.stringify(error, null, 2);
+
+    return buildResponseBody(500, body);
+  }
+}
