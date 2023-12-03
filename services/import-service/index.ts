29e562b4bbbbb4468ffb8ab400d444da7aa7e92f
anastasiya.tsymanovich <anastasiya.tsymanovich@zealicsolutions.com>
2023-12-03 11:57:14 +0300

    feat(task-5): create importProductsFile lambda task 5.2

--- services/import-service/index.ts
+++ services/import-service/index.ts
@@ -1,14 +1,56 @@
 import { Construct } from '@aws-cdk/core';
+import * as apigateway from '@aws-cdk/aws-apigateway';
+import * as lambda from '@aws-cdk/aws-lambda';
 import * as s3 from '@aws-cdk/aws-s3';
+import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
+import * as path from 'path';
+import { JsonSchemaType } from '@aws-cdk/aws-apigateway';
 
 export class ImportService extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // Import bucket
-    // const importBucket = new s3.Bucket(this, 'tsimanovich-import', {
-    //   bucketName: 'tsimanovich-import', // /uploaded
-    //   publicReadAccess: true,
-    // });
+    const importBucket = s3.Bucket.fromBucketName(
+      this,
+      'tsimanovich-import',
+      'tsimanovich-import',
+    );
+
+    // Import Products File
+    const importProductsFileBucket = new s3.Bucket(this, 'ImportProductsFile');
+
+    const importProductsFileHandler = new NodejsFunction(this, 'importProductsFileHandler', {
+      memorySize: 1024,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: path.join(__dirname, `./lambdas/importProductsFile/index.ts`),
+      handler: 'handler',
+      environment: {
+        BUCKET: importProductsFileBucket.bucketName,
+        UPLOAD_BUCKET: importBucket.bucketName,
+      }
+    });
+
+    importProductsFileBucket.grantReadWrite(importProductsFileHandler);
+
+    importBucket.grantReadWrite(importProductsFileHandler);
+
+    // Products API
+    const api = new apigateway.RestApi(this, 'import-api', {
+      restApiName: 'import',
+      description: 'This service import files.',
+      defaultCorsPreflightOptions: {
+        allowHeaders: ['Content-Type'],
+        allowMethods: apigateway.Cors.ALL_METHODS,
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+      },
+    });
+
+    const importApi = api.root.addResource('import');
+
+    const importProductsFileIntegration = new apigateway.LambdaIntegration(importProductsFileHandler, {
+      requestTemplates: { 'application/json': '{ "statusCode": "200" }' },
+    });
+
+    importApi.addMethod('GET', importProductsFileIntegration);
   }
 }
---
+++ services/import-service/lambdas/importProductsFile/index.ts
@@ -0,0 +1,31 @@
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
+import { buildResponseBody } from '../../../../utils/buildResponseBody.util';
+
+const client = new S3Client();
+
+export const handler = async function(event: any) {
+  console.log(`event: ${event}`);
+
+  const name = event.queryStringParameters?.name;
+  
+  try {
+    // Get signed URL from S3
+    const putObjectParams = {
+      Bucket: process.env.UPLOAD_BUCKET,
+      Key: `uploaded/${name}.csv`,
+      ContentType: 'application/json',
+    };
+
+    const command = new PutObjectCommand(putObjectParams);
+
+    const signedUrl = await getSignedUrl(client, command);
+
+    return buildResponseBody(200, signedUrl);
+  } catch(error: any) {
+    const body = error.stack || JSON.stringify(error, null, 2);
+
+    return buildResponseBody(500, body);
+  }
+}
